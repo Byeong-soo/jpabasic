b53d5ce8da9c25eec07d63e7825c010c9045cd2d
ubs <ubs4939@naver.com>
2022-05-13 14:07:02 +0900

    영속성 전의와 고아객체

---
+++ src/main/java/hellojpa/CascadeJpa.java
@@ -0,0 +1,47 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Arrays;
+
+public class CascadeJpa {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent  = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+            //cascade 를 사용하면 persist(child) 코드 적지않아도 자동으로 해줌.
+            em.persist(child1);
+            em.persist(child2);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
